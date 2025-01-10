f574d5bc1f3a7d222681a5d9612d2f6a852511df
iqbalarrahman08 <iqbalarrahman80@gmail.com>
2025-01-10 21:34:17 +0700

    update routes file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const app = require("./app");
 const port = 4000;
 
@@ -9,15 +8,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`running on port ${port}`);
 });
-=======
-const app = require("./app");
-const port = 4000;
-
-app.get("/", (req, res) => {
-  res.send(`server berjalan di port ${port}`);
-});
-
-app.listen(port, () => {
-  console.log(`running on port ${port}`);
-});
->>>>>>> b5a97a875d677f2ea7415ac64ad45b7e11c273f3
