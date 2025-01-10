99830152b23488860d2f9e27a0f8fbc2533cece9
iqbalarrahman08 <iqbalarrahman80@gmail.com>
2025-01-10 21:31:32 +0700

    update app

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -11,17 +10,3 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/payment", paymentRouter);
 
 module.exports = app;
-=======
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const paymentRouter = require("./router");
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api/payment", paymentRouter);
-
-module.exports = app;
->>>>>>> b5a97a875d677f2ea7415ac64ad45b7e11c273f3
